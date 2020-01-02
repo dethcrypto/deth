4c3cf768dacba0aad49aec8cd45d775f4aea9f84
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2020-01-02 17:49:30 +0100

    Add MachineWord (#20)
    * Add MachineWord with incomplete tests
    
    * Add more test cases
    
    * Add Parity test cases
    
    * Add comprehensive geth tests
    
    * Reorder geth tests
    
    * Remove addModulo and multiplyModulo as they are redundant
    
    * Add more parity tests
    
    * Remove keccak functionality
    
    * Remove .only from tests
    
    * Fix linter errors

---
+++ packages/evm/src/MachineWord.ts
@@ -0,0 +1,208 @@
+import BN from 'bn.js'
+
+const TWO_POW256 = new BN('1' + '0'.repeat(64), 16)
+const MAX_256 = new BN('f'.repeat(64), 16)
+
+export class MachineWord {
+  private value: BN
+  constructor (value: BN) {
+    this.value = value.clone()
+  }
+
+  static ZERO = new MachineWord(new BN(0))
+  static ONE = new MachineWord(new BN(1))
+  static MAX = new MachineWord(MAX_256)
+
+  static fromBoolean (value: boolean) {
+    return value ? MachineWord.ONE : MachineWord.ZERO
+  }
+
+  static fromHexString (value: string) {
+    return new MachineWord(new BN(value, 16))
+  }
+
+  toHexString () {
+    return this.value.toString(16, 64)
+  }
+
+  private get signed () {
+    return this.value.fromTwos(256)
+  }
+
+  add (other: MachineWord) {
+    const result = this.value
+      .add(other.value)
+      .mod(TWO_POW256)
+    return new MachineWord(result)
+  }
+
+  subtract (other: MachineWord) {
+    const result = this.value
+      .sub(other.value)
+      .toTwos(256)
+    return new MachineWord(result)
+  }
+
+  multiply (other: MachineWord) {
+    const result = this.value
+      .mul(other.value)
+      .mod(TWO_POW256)
+    return new MachineWord(result)
+  }
+
+  unsignedDivide (other: MachineWord) {
+    if (other.isZero()) {
+      return MachineWord.ZERO
+    }
+    const result = this.value
+      .div(other.value)
+    return new MachineWord(result)
+  }
+
+  signedDivide (other: MachineWord) {
+    if (other.isZero()) {
+      return MachineWord.ZERO
+    }
+    const result = this.signed
+      .div(other.signed)
+      .toTwos(256)
+    return new MachineWord(result)
+  }
+
+  unsignedModulo (other: MachineWord) {
+    if (other.isZero()) {
+      return MachineWord.ZERO
+    }
+    const result = this.value
+      .mod(other.value)
+    return new MachineWord(result)
+  }
+
+  signedModulo (other: MachineWord) {
+    if (other.isZero()) {
+      return MachineWord.ZERO
+    }
+
+    const intermediate = this.signed.abs().mod(other.signed.abs())
+    const result = this.signed.isNeg()
+      ? intermediate.ineg().toTwos(256)
+      : intermediate
+    return new MachineWord(result)
+  }
+
+  exponentiate (power: MachineWord) {
+    if (power.isZero()) {
+      return MachineWord.ONE
+    }
+    if (this.isZero()) {
+      return MachineWord.ZERO
+    }
+    const redBase = this.value.toRed(BN.red(TWO_POW256))
+    const result = redBase.redPow(power.value)
+    return new MachineWord(result)
+  }
+
+  extendSign (bytes: MachineWord) {
+    if (bytes.value.gten(31)) {
+      return this
+    }
+    const signBit = bytes.value
+      .muln(8)
+      .iaddn(7)
+      .toNumber()
+    const mask = new BN(1).ishln(signBit).isubn(1)
+    const result = this.value.testn(signBit)
+      ? this.value.or(mask.notn(256))
+      : this.value.and(mask)
+    return new MachineWord(result)
+  }
+
+  unsignedLessThan (other: MachineWord) {
+    return this.value.lt(other.value)
+  }
+
+  signedLessThan (other: MachineWord) {
+    return this.signed.lt(other.signed)
+  }
+
+  unsignedGreaterThan (other: MachineWord) {
+    return this.value.gt(other.value)
+  }
+
+  signedGreaterThan (other: MachineWord) {
+    return this.signed.gt(other.signed)
+  }
+
+  equals (other: MachineWord) {
+    return this.value.eq(other.value)
+  }
+
+  isZero () {
+    return this.value.isZero()
+  }
+
+  and (other: MachineWord) {
+    const result = this.value.and(other.value)
+    return new MachineWord(result)
+  }
+
+  or (other: MachineWord) {
+    const result = this.value.or(other.value)
+    return new MachineWord(result)
+  }
+
+  xor (other: MachineWord) {
+    const result = this.value.xor(other.value)
+    return new MachineWord(result)
+  }
+
+  not () {
+    const result = this.value.notn(256)
+    return new MachineWord(result)
+  }
+
+  getByte (position: MachineWord) {
+    if (position.value.gten(32)) {
+      return MachineWord.ZERO
+    }
+    // for some reason the actual type of result is number!
+    const result = this.value
+      .shrn((31 - position.value.toNumber()) * 8)
+      .andln(0xff)
+    return new MachineWord(new BN(result))
+  }
+
+  shiftLeft (by: MachineWord) {
+    if (by.value.gten(256)) {
+      return MachineWord.ZERO
+    }
+    const result = this.value.shln(by.value.toNumber()).iand(MAX_256)
+    return new MachineWord(result)
+  }
+
+  logicalShiftRight (by: MachineWord) {
+    if (by.value.gten(256)) {
+      return MachineWord.ZERO
+    }
+    const result = this.value.shrn(by.value.toNumber())
+    return new MachineWord(result)
+  }
+
+  arithmeticShiftRight (by: MachineWord) {
+    const isSigned = this.value.testn(255)
+    if (by.value.gten(256)) {
+      if (isSigned) {
+        return MachineWord.MAX
+      } else {
+        return MachineWord.ZERO
+      }
+    }
+    const result = this.value.shrn(by.value.toNumber())
+    if (isSigned) {
+      const shiftedOutWidth = 255 - by.value.toNumber()
+      const mask = MAX_256.shrn(shiftedOutWidth).shln(shiftedOutWidth)
+      result.ior(mask)
+    }
+    return new MachineWord(result)
+  }
+}
---
+++ packages/evm/test/MachineWord/parity.test.ts
@@ -0,0 +1,511 @@
+import { expect } from 'chai'
+import { MachineWord } from '../../src/MachineWord'
+
+const negative = (value: string) => MachineWord.ZERO
+  .subtract(MachineWord.fromHexString(value))
+  .toHexString()
+
+describe('MachineWord - parity', () => {
+  testBinOp('add', [
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L51
+    {
+      value: negative('1'),
+      other: negative('1'),
+      result: negative('2'),
+    },
+  ])
+
+  testBinOp('subtract', [
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L465
+    {
+      value: '12365124623',
+      other: '654321',
+      result: '12364ad0302',
+    },
+  ])
+
+  testBinOp('multiply', [
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L447
+    {
+      value: '12365124623',
+      other: '654321',
+      result: '734349397b853383',
+    },
+  ])
+
+  testBinOp('unsignedDivide', [
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L483
+    {
+      value: '12365124623',
+      other: '654321',
+      result: '2e0ac',
+    },
+  ])
+
+  testBinOp('signedDivide', [
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L557
+    {
+      value: '12365124623',
+      other: '654322',
+      result: '2e0ac',
+    },
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L558
+    {
+      value: '12365124623',
+      other: '0',
+      result: '0',
+    },
+  ])
+
+  testBinOp('unsignedModulo', [
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L519
+    {
+      value: '12365124623',
+      other: '654322',
+      result: '76b4b',
+    },
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L520
+    {
+      value: '12365124623',
+      other: '0',
+      result: '0',
+    },
+  ])
+
+  testBinOp('signedModulo', [
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L538
+    {
+      value: '12365124623',
+      other: '654322',
+      result: '76b4b',
+    },
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L539
+    {
+      value: '12365124623',
+      other: '0',
+      result: '0',
+    },
+  ])
+
+  testBinOp('exponentiate', [
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L576
+    {
+      value: '12365124623',
+      other: '16',
+      result: '90fd23767b60204c3d6fc8aec9e70a42a3f127140879c133a20129a597ed0c59',
+    },
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L577
+    {
+      value: '12365124623',
+      other: '1',
+      result: '12365124623',
+    },
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L578
+    {
+      value: '12365124623',
+      other: '0',
+      result: '1',
+    },
+  ])
+
+  testBinOp('extendSign', [
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L701
+    {
+      value: 'fff',
+      other: '2',
+      result: 'fff',
+    },
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L702
+    {
+      value: 'ff',
+      other: '20',
+      result: 'ff',
+    },
+  ])
+
+  testBinOp('unsignedLessThan', [
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L596
+    {
+      value: '12365124623',
+      other: '16',
+      result: false,
+    },
+  ])
+
+  testBinOp('signedLessThan', [
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L617
+    {
+      value: '10',
+      other: negative('10'),
+      result: false,
+    },
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L619
+    {
+      value: negative('10'),
+      other: '10',
+      result: true,
+    },
+  ])
+
+  testBinOp('unsignedGreaterThan', [
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L597
+    {
+      value: '12365124623',
+      other: '16',
+      result: true,
+    },
+  ])
+
+  testBinOp('signedGreaterThan', [
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L618
+    {
+      value: '10',
+      other: negative('10'),
+      result: true,
+    },
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L620
+    {
+      value: negative('10'),
+      other: '10',
+      result: false,
+    },
+  ])
+
+  testBinOp('equals', [
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L598
+    {
+      value: '12365124623',
+      other: '16',
+      result: false,
+    },
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L599
+    {
+      value: '1523541235',
+      other: '1523541235',
+      result: true,
+    },
+  ])
+
+  /*
+    Storage:
+      0: and(0ff0, ff)
+      1: or(0ff0, ff)
+      2: xor(0ff0, ff)
+      3: izZero(0)
+      4: izZero(not(0))
+      5: not(0)
+   */
+
+  testUnaryOp('isZero', [
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L641
+    {
+      value: '0',
+      result: true,
+    },
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L642
+    {
+      value: negative('1'),
+      result: false,
+    },
+  ])
+
+  testBinOp('and', [
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L638
+    {
+      value: '0ff0',
+      other: '00ff',
+      result: '00f0',
+    },
+  ])
+
+  testBinOp('or', [
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L639
+    {
+      value: '0ff0',
+      other: '00ff',
+      result: '0fff',
+    },
+  ])
+
+  testBinOp('xor', [
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L640
+    {
+      value: '0ff0',
+      other: '00ff',
+      result: '0f0f',
+    },
+  ])
+
+  testUnaryOp('not', [
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L643
+    {
+      value: '0',
+      result: negative('1'),
+    },
+  ])
+
+  testBinOp('getByte', [
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L682
+    {
+      value: 'ffff',
+      other: 'f0',
+      result: '00',
+    },
+    // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L683
+    {
+      value: 'fff',
+      other: '1f',
+      result: 'ff',
+    },
+  ])
+
+  // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L858
+  testBinOp('shiftLeft', [
+    {
+      value: '1',
+      other: '0',
+      result: '1',
+    },
+    {
+      value: '1',
+      other: '1',
+      result: '2',
+    },
+    {
+      value: '1',
+      other: 'ff',
+      result: '8000000000000000000000000000000000000000000000000000000000000000',
+    },
+    {
+      value: '1',
+      other: '100',
+      result: '0',
+    },
+    {
+      value: '1',
+      other: '101',
+      result: '0',
+    },
+    {
+      value: negative('1'),
+      other: '0',
+      result: negative('1'),
+    },
+    {
+      value: negative('1'),
+      other: '1',
+      result: 'fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe',
+    },
+    {
+      value: negative('1'),
+      other: 'ff',
+      result: '8000000000000000000000000000000000000000000000000000000000000000',
+    },
+    {
+      value: negative('1'),
+      other: '100',
+      result: '0',
+    },
+    {
+      value: '0',
+      other: '1',
+      result: '0',
+    },
+    {
+      value: '7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+      other: '1',
+      result: 'fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe',
+    },
+  ])
+
+  // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L928
+  testBinOp('logicalShiftRight', [
+    {
+      value: '0000000000000000000000000000000000000000000000000000000000000001',
+      other: '00',
+      result: '0000000000000000000000000000000000000000000000000000000000000001',
+    },
+    {
+      value: '0000000000000000000000000000000000000000000000000000000000000001',
+      other: '01',
+      result: '0000000000000000000000000000000000000000000000000000000000000000',
+    },
+    {
+      value: '8000000000000000000000000000000000000000000000000000000000000000',
+      other: '01',
+      result: '4000000000000000000000000000000000000000000000000000000000000000',
+    },
+    {
+      value: '8000000000000000000000000000000000000000000000000000000000000000',
+      other: 'ff',
+      result: '0000000000000000000000000000000000000000000000000000000000000001',
+    },
+    {
+      value: '8000000000000000000000000000000000000000000000000000000000000000',
+      other: '0100',
+      result: '0000000000000000000000000000000000000000000000000000000000000000',
+    },
+    {
+      value: '8000000000000000000000000000000000000000000000000000000000000000',
+      other: '0101',
+      result: '0000000000000000000000000000000000000000000000000000000000000000',
+    },
+    {
+      value: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+      other: '00',
+      result: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+    },
+    {
+      value: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+      other: '01',
+      result: '7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+    },
+    {
+      value: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+      other: 'ff',
+      result: '0000000000000000000000000000000000000000000000000000000000000001',
+    },
+    {
+      value: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+      other: '0100',
+      result: '0000000000000000000000000000000000000000000000000000000000000000',
+    },
+    {
+      value: '0000000000000000000000000000000000000000000000000000000000000000',
+      other: '01',
+      result: '0000000000000000000000000000000000000000000000000000000000000000',
+    },
+  ])
+
+  // https://github.com/paritytech/parity-ethereum/blob/dabfa2c6/ethcore/evm/src/tests.rs#L998
+  testBinOp('arithmeticShiftRight', [
+    {
+      value: '0000000000000000000000000000000000000000000000000000000000000001',
+      other: '00',
+      result: '0000000000000000000000000000000000000000000000000000000000000001',
+    },
+    {
+      value: '0000000000000000000000000000000000000000000000000000000000000001',
+      other: '01',
+      result: '0000000000000000000000000000000000000000000000000000000000000000',
+    },
+    {
+      value: '8000000000000000000000000000000000000000000000000000000000000000',
+      other: '01',
+      result: 'c000000000000000000000000000000000000000000000000000000000000000',
+    },
+    {
+      value: '8000000000000000000000000000000000000000000000000000000000000000',
+      other: 'ff',
+      result: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+    },
+    {
+      value: '8000000000000000000000000000000000000000000000000000000000000000',
+      other: '0100',
+      result: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+    },
+    {
+      value: '8000000000000000000000000000000000000000000000000000000000000000',
+      other: '0101',
+      result: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+    },
+    {
+      value: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+      other: '00',
+      result: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+    },
+    {
+      value: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+      other: '01',
+      result: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+    },
+    {
+      value: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+      other: 'ff',
+      result: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+    },
+    {
+      value: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+      other: '0100',
+      result: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+    },
+    {
+      value: '0000000000000000000000000000000000000000000000000000000000000000',
+      other: '01',
+      result: '0000000000000000000000000000000000000000000000000000000000000000',
+    },
+    {
+      value: '4000000000000000000000000000000000000000000000000000000000000000',
+      other: 'fe',
+      result: '0000000000000000000000000000000000000000000000000000000000000001',
+    },
+    {
+      value: '7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+      other: 'f8',
+      result: '000000000000000000000000000000000000000000000000000000000000007f',
+    },
+    {
+      value: '7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+      other: 'fe',
+      result: '0000000000000000000000000000000000000000000000000000000000000001',
+    },
+    {
+      value: '7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+      other: 'ff',
+      result: '0000000000000000000000000000000000000000000000000000000000000000',
+    },
+    {
+      value: '7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+      other: '0100',
+      result: '0000000000000000000000000000000000000000000000000000000000000000',
+    },
+  ])
+})
+
+interface UnaryTestCase {
+  value: string,
+  result: string | boolean,
+}
+
+interface BinaryTestCase {
+  value: string,
+  other: string,
+  result: string | boolean,
+}
+
+function testUnaryOp (method: keyof MachineWord, testCases: UnaryTestCase[]) {
+  describe('MachineWord.' + method, () => {
+    testCases.forEach((testCase, index) => {
+      it(`${method} test #${index + 1}`, () => {
+        const a = MachineWord.fromHexString(testCase.value)
+        const result = (a as any)[method]()
+        if (result instanceof MachineWord && typeof testCase.result === 'string') {
+          expect(result.toHexString()).to.equal(testCase.result.padStart(64, '0'))
+        } else if (typeof result === 'boolean' && typeof testCase.result === 'boolean') {
+          expect(result).to.equal(testCase.result)
+        } else {
+          throw new Error('Invalid result!')
+        }
+      })
+    })
+  })
+}
+
+function testBinOp (method: keyof MachineWord, testCases: BinaryTestCase[]) {
+  describe('MachineWord.' + method, () => {
+    testCases.forEach((testCase, index) => {
+      it(`${method} test #${index + 1}`, () => {
+        const a = MachineWord.fromHexString(testCase.value)
+        const b = MachineWord.fromHexString(testCase.other)
+        const result = (a as any)[method](b)
+        if (result instanceof MachineWord && typeof testCase.result === 'string') {
+          expect(result.toHexString()).to.equal(testCase.result.padStart(64, '0'))
+        } else if (typeof result === 'boolean' && typeof testCase.result === 'boolean') {
+          expect(result).to.equal(testCase.result)
+        } else {
+          throw new Error('Invalid result!')
+        }
+      })
+    })
+  })
+}
