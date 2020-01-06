6c5b66b84daee9479484fb490cd1cd8acd427dc2
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2020-01-06 10:32:14 +0100

    Add SWAP* tests

--- packages/evm/test/evm/opcodes/dup.test.ts
+++ packages/evm/test/evm/opcodes/dup.test.ts
@@ -4,35 +4,31 @@ import { GasCost } from '../../../src/evm/opcodes/gasCosts'
 import { StackUnderflow } from '../../../src/evm/errors'
 
 describe('DUP* opcodes', () => {
-  for (let i = 1; i <= 16; i++) {
-    testDupN(i)
+  const stack = new Array(16).fill(0)
+    .map((value, index) => (16 - index).toString(16).padStart(64, '0'))
+
+  const assembly = stack
+    .map((value) => `PUSH32 ${value}`)
+    .join(' ')
+
+  for (let n = 1; n <= 16; n++) {
+    describe(`DUP${n}`, () => {
+      it('duplicates the value on the stack', () => {
+        const result = executeAssembly(`${assembly} DUP${n}`)
+        expect(result.stack.pop().toHexString()).to.equal(stack[stack.length - n])
+      })
+
+      for (let i = 0; i < n; i++) {
+        it(`fails for stack of depth ${i}`, () => {
+          const result = executeAssembly(`${'PUSH1 00 '.repeat(i)} DUP${n}`)
+          expect(result.error).to.be.instanceOf(StackUnderflow)
+        })
+      }
+
+      it(`uses ${GasCost.VERYLOW} gas`, () => {
+        const result = executeAssembly(`${assembly} DUP${n}`)
+        expect(result.gasUsed - GasCost.VERYLOW * 16).to.equal(GasCost.VERYLOW)
+      })
+    })
   }
 })
-
-const stack = new Array(16).fill(0)
-  .map((value, index) => (16 - index).toString(16).padStart(64, '0'))
-
-const assembly = stack
-  .map((value) => `PUSH32 ${value}`)
-  .join(' ')
-
-function testDupN (n: number) {
-  describe(`DUP${n}`, () => {
-    it('duplicates the value on the stack', () => {
-      const result = executeAssembly(`${assembly} DUP${n}`)
-      expect(result.stack.pop().toHexString()).to.equal(stack[stack.length - n])
-    })
-
-    for (let i = 0; i < n; i++) {
-      it(`fails for stack of depth ${i}`, () => {
-        const result = executeAssembly(`${'PUSH1 00 '.repeat(i)} DUP${n}`)
-        expect(result.error).to.be.instanceOf(StackUnderflow)
-      })
-    }
-
-    it(`uses ${GasCost.VERYLOW} gas`, () => {
-      const result = executeAssembly(`${assembly} DUP${n}`)
-      expect(result.gasUsed - GasCost.VERYLOW * 16).to.equal(GasCost.VERYLOW)
-    })
-  })
-}
--- packages/evm/test/evm/opcodes/push.test.ts
+++ packages/evm/test/evm/opcodes/push.test.ts
@@ -3,27 +3,23 @@ import { executeAssembly } from '../executeAssembly'
 import { GasCost } from '../../../src/evm/opcodes/gasCosts'
 
 describe('PUSH* opcodes', () => {
-  for (let i = 1; i <= 32; i++) {
-    testPushN(i)
+  for (let n = 1; n <= 32; n++) {
+    describe(`PUSH${n}`, () => {
+      const bytes = new Array(n).fill(0)
+        .map((value, index) => (index + 1).toString(16).padStart(2, '0'))
+        .join('')
+
+      it('pushes a value onto the stack', () => {
+        const result = executeAssembly(`PUSH${n} ${bytes}`)
+        expect(result.stack.pop().toHexString()).to.equal(bytes.padStart(64, '0'))
+      })
+
+      it(`uses ${GasCost.VERYLOW} gas`, () => {
+        const result = executeAssembly(`PUSH${n} ${bytes}`)
+        expect(result.gasUsed).to.equal(GasCost.VERYLOW)
+      })
+
+      xit('results in stackoverflow after 1024 items')
+    })
   }
 })
-
-function testPushN (n: number) {
-  describe(`PUSH${n}`, () => {
-    const bytes = new Array(n).fill(0)
-      .map((value, index) => (index + 1).toString(16).padStart(2, '0'))
-      .join('')
-
-    it('pushes a value onto the stack', () => {
-      const result = executeAssembly(`PUSH${n} ${bytes}`)
-      expect(result.stack.pop().toHexString()).to.equal(bytes.padStart(64, '0'))
-    })
-
-    it(`uses ${GasCost.VERYLOW} gas`, () => {
-      const result = executeAssembly(`PUSH${n} ${bytes}`)
-      expect(result.gasUsed).to.equal(GasCost.VERYLOW)
-    })
-
-    xit('results in stackoverflow after 1024 items')
-  })
-}
