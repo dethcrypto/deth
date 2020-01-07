0507d86344bb00a80855c642f87e0fad79010264
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2020-01-07 22:33:24 +0100

    Fully implement memory tests

--- packages/evm/test/evm/opcodes/memory.test.ts
+++ packages/evm/test/evm/opcodes/memory.test.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai'
 import { executeAssembly } from '../executeAssembly'
 import { GasCost } from '../../../src/evm/opcodes'
+import { StackUnderflow } from '../../../src/evm/errors'
 
 describe('Memory opcodes', () => {
   describe('MSIZE', () => {
@@ -24,27 +25,134 @@ describe('Memory opcodes', () => {
     })
   })
 
-  it('MSTORE', () => {
-    xit(`uses ${GasCost.VERYLOW} gas`)
-    xit('causes memory expansion')
-    xit('stores data in memory')
-    xit('fails for stack of depth 1')
-    xit('fails for stack of depth 0')
+  const memoryGas = (bytes: number) => memoryGasWords(Math.ceil(bytes / 32))
+  const memoryGasWords = (words: number) => words * 3 + Math.floor(words * words / 512)
+
+  describe('MSTORE', () => {
+    it(`uses ${GasCost.VERYLOW} gas and causes memory expansion`, () => {
+      const result = executeAssembly(`
+        PUSH2 BABA
+        DUP1
+        DUP1
+        PUSH1 00
+        MSTORE
+        PUSH3 100001
+        MSTORE
+        PUSH1 01
+        MSTORE
+      `)
+      expect(result.error).to.equal(undefined)
+      expect(result.gasUsed).to.equal(
+        GasCost.VERYLOW * 9 + // PUSH, DUP and MSTORE
+        memoryGas(0x100001 + 32)
+      )
+    })
+
+    it('stores data in memory', () => {
+      const word = '1234567890abcdef'.repeat(4)
+      const result = executeAssembly(`
+        PUSH32 ${word}
+        PUSH1 00
+        MSTORE
+        PUSH1 00
+        MLOAD
+      `)
+      expect(result.error).to.equal(undefined)
+      expect(result.stack.pop().toHexString()).to.equal(word)
+    })
+
+    it('fails for stack of depth 1', () => {
+      const result = executeAssembly('PUSH1 00 MSTORE')
+      expect(result.error).to.be.instanceOf(StackUnderflow)
+    })
+
+    it('fails for stack of depth 0', () => {
+      const result = executeAssembly('MSTORE')
+      expect(result.error).to.be.instanceOf(StackUnderflow)
+    })
   })
 
-  xit('MSTOR8', () => {
-    xit(`uses ${GasCost.VERYLOW} gas`)
-    xit('causes memory expansion')
-    xit('stores data in memory')
-    xit('fails for stack of depth 1')
-    xit('fails for stack of depth 0')
+  describe('MSTORE8', () => {
+    it(`uses ${GasCost.VERYLOW} gas and causes memory expansion`, () => {
+      const result = executeAssembly(`
+        PUSH2 BABA
+        DUP1
+        DUP1
+        PUSH1 00
+        MSTORE8
+        PUSH3 100001
+        MSTORE8
+        PUSH1 01
+        MSTORE8
+      `)
+      expect(result.error).to.equal(undefined)
+      expect(result.gasUsed).to.equal(
+        GasCost.VERYLOW * 9 + // PUSH, DUP and MSTORE8
+        memoryGas(0x100001 + 1)
+      )
+    })
+
+    it('stores a single byte in memory', () => {
+      const word = '1234567890abcdef'.repeat(4)
+      const result = executeAssembly(`
+        PUSH32 ${word}
+        PUSH1 00
+        MSTORE8
+        PUSH1 00
+        MLOAD
+      `)
+      expect(result.error).to.equal(undefined)
+      expect(result.stack.pop().toHexString()).to.equal('ef'.padEnd(64, '0'))
+    })
+
+    it('fails for stack of depth 1', () => {
+      const result = executeAssembly('PUSH1 00 MSTORE8')
+      expect(result.error).to.be.instanceOf(StackUnderflow)
+    })
+
+    it('fails for stack of depth 0', () => {
+      const result = executeAssembly('MSTORE8')
+      expect(result.error).to.be.instanceOf(StackUnderflow)
+    })
   })
 
-  xit('MLOAD', () => {
-    xit(`uses ${GasCost.VERYLOW} gas`)
-    xit('causes memory expansion')
-    xit('loads data from memory')
-    xit('fails for stack of depth 1')
-    xit('fails for stack of depth 0')
+  describe('MLOAD', () => {
+    it(`uses ${GasCost.VERYLOW} gas and causes memory expansion`, () => {
+      const result = executeAssembly(`
+        PUSH1 00
+        MLOAD
+        PUSH3 100001
+        MLOAD
+        PUSH1 01
+        MLOAD
+      `)
+      expect(result.error).to.equal(undefined)
+      expect(result.gasUsed).to.equal(
+        GasCost.VERYLOW * 6 + // PUSH, DUP and MLOAD
+        memoryGas(0x100001 + 32)
+      )
+    })
+
+    it('loads data from memory', () => {
+      const result = executeAssembly(`
+        PUSH32 ${'e'.repeat(64)}
+        PUSH1 00
+        MSTORE
+        PUSH32 ${'b'.repeat(64)}
+        PUSH1 20
+        MSTORE
+        PUSH1 10
+        MLOAD
+      `)
+      expect(result.error).to.equal(undefined)
+      const topOfStack = result.stack.pop().toHexString()
+      const expected = 'e'.repeat(32) + 'b'.repeat(32)
+      expect(topOfStack).to.equal(expected)
+    })
+
+    it('fails for stack of depth 0', () => {
+      const result = executeAssembly('MLOAD')
+      expect(result.error).to.be.instanceOf(StackUnderflow)
+    })
   })
 })
