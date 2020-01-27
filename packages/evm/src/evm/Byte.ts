7fa4c2dd9db932b2163f41a7aa6ac1e5558defad
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2020-01-27 19:20:36 +0100

    Remove unused byte function

--- packages/evm/src/evm/Byte.ts
+++ packages/evm/src/evm/Byte.ts
@@ -1,15 +1,1 @@
 export type Byte = number & { __type__: 'Byte' }
-export const Byte = {
-  fromNumber (value: number) {
-    if ((value & 0xFF) !== value) {
-      throw new TypeError(`Cannot cast ${value} to byte`)
-    }
-    return value as Byte
-  },
-  fromHex (value: string) {
-    if (!/^[\da-f]{2}$/i.test(value)) {
-      throw new TypeError(`Cannot cast "${value}" to byte`)
-    }
-    return parseInt(value, 16) as Byte
-  },
-}
