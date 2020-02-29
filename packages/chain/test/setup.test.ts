38b71776841a8e3afab99b6ab9816b75ef359af5
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2020-02-29 13:11:01 +0100

    Simplify chain dependencies

--- packages/chain/test/setup.test.ts
+++ packages/chain/test/setup.test.ts
@@ -1,8 +1,4 @@
 import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
-import sinonChai from 'sinon-chai'
-import chaiSubset from 'chai-subset'
 
 chai.use(chaiAsPromised)
-chai.use(sinonChai)
-chai.use(chaiSubset)
