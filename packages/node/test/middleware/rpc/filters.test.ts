326f447ed039051dc19ac66d1a844d8c18fbdfec
Krzysztof Kaczor <krzkaczor@gmail.com>
2020-03-01 18:58:07 +0100

    rpc -> filters test

---
+++ packages/node/test/middleware/rpc/filters.test.ts
@@ -0,0 +1,20 @@
+import { expect } from 'chai'
+
+import { makeRpcCall, unwrapRpcResponse } from './common'
+import { buildTestApp } from '../../buildTestApp'
+
+describe('rpc -> filters', () => {
+  it('creates block filters', async () => {
+    const app = await buildTestApp()
+
+    const filterId = unwrapRpcResponse(await makeRpcCall(app, 'eth_newBlockFilter', []))
+
+    expect(filterId).to.be.string
+
+    await app.services.chain.mineBlock()
+
+    const changes = unwrapRpcResponse(await makeRpcCall(app, 'eth_getFilterChanges', [filterId]))
+
+    expect(changes).to.have.length(1)
+  })
+})
