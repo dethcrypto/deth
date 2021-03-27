051ba79245c03ddadfc3d746ad95c02b7d96da59
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-03-27 12:58:00 +0100

    Fix rlpDecode

--- packages/evm/src/rlp/rlpDecode.ts
+++ packages/evm/src/rlp/rlpDecode.ts
@@ -13,51 +13,60 @@ function rlpDecodePartial(value: Bytes): [RlpSerializable, Bytes] {
 
   if (firstByte < 128) {
     check(value.length >= 1)
-    return [value.slice(0, 1), value.slice(1, value.length)]
+    return split(value, 0, 1)
   }
 
   if (firstByte < 128 + 56) {
     const length = firstByte - 128
     check(value.length >= 1 + length)
-    return [value.slice(1, 1 + length), value.slice(1 + length, value.length)]
+    check(length !== 1 || value.get(1) >= 128)
+    return split(value, 1, 1 + length)
   }
 
   if (firstByte < 192) {
-    const lengthOfLength = firstByte - 183
-    check(value.length >= 1 + lengthOfLength)
-    const length = value.slice(1, 1 + lengthOfLength).toNumber()
-    const end = 1 + lengthOfLength + length
-    check(length >= 56 && value.length >= end)
-    return [
-      value.slice(1 + lengthOfLength, end),
-      value.slice(end, value.length),
-    ]
+    return decodeLonger(value, firstByte - 183)
   }
 
   if (firstByte < 248) {
     const length = firstByte - 192
-    let rest = value.slice(1, 1 + length)
-    const result: RlpSerializable[] = []
-    while (rest.length > 0) {
-      const partial = rlpDecodePartial(rest)
-      result.push(partial[0])
-      rest = partial[1]
-    }
-    return [result, value.slice(1 + length, value.length)]
+    check(value.length >= 1 + length)
+    const [toDecode, rest] = split(value, 1, 1 + length)
+    return [decodeItems(toDecode), rest]
   }
 
-  const lengthOfLength = firstByte - 247
+  const [toDecode, rest] = decodeLonger(value, firstByte - 247)
+  return [decodeItems(toDecode), rest]
+}
+
+function split(value: Bytes, start: number, middle: number): [Bytes, Bytes] {
+  return [value.slice(start, middle), value.slice(middle, value.length)]
+}
+
+function decodeNumber(value: Bytes) {
+  if (value.length === 0) {
+    return 0
+  }
+  check(value.get(0) !== 0)
+  return value.toNumber()
+}
+
+function decodeLonger(value: Bytes, lengthOfLength: number) {
   check(value.length >= 1 + lengthOfLength)
-  const length = value.slice(1, 1 + lengthOfLength).toNumber()
+  const length = decodeNumber(value.slice(1, 1 + lengthOfLength))
   const end = 1 + lengthOfLength + length
-  let rest = value.slice(1 + lengthOfLength, end)
+  check(length >= 56 && value.length >= end)
+  return split(value, 1 + lengthOfLength, end)
+}
+
+function decodeItems(value: Bytes) {
+  let rest = value
   const result: RlpSerializable[] = []
   while (rest.length > 0) {
     const partial = rlpDecodePartial(rest)
     result.push(partial[0])
     rest = partial[1]
   }
-  return [result, value.slice(end, value.length)]
+  return result
 }
 
 function check(value: boolean): asserts value {
