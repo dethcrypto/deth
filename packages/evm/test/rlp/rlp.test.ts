c2bc3dbee958578a26bc0e0714ad6961da089269
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-03-27 12:59:03 +0100

    Fix rlpDecode (#92)

--- packages/evm/test/rlp/rlp.test.ts
+++ packages/evm/test/rlp/rlp.test.ts
@@ -1,42 +1,80 @@
 import { expect } from 'chai'
-import testCasesJSON from './testCases.json'
+import testCasesEthers from './ethers.json'
+import testCasesEthValid from './eth-valid.json'
+import testCasesEthInvalid from './eth-invalid.json'
 import { Bytes } from '../../src/Bytes'
 import { RlpSerializable, rlpEncode, rlpDecode } from '../../src/rlp'
-
-const testCases = testCasesJSON.map((test) => ({
-  name: test.name,
-  decoded: mapDecoded(test.decoded),
-  encoded: mapHex(test.encoded),
-}))
-
-type Decoded = string | Decoded[]
-
-function mapDecoded(value: Decoded): RlpSerializable {
-  if (Array.isArray(value)) {
-    return value.map(mapDecoded)
-  } else {
-    return mapHex(value)
-  }
-}
-
-function mapHex(value: string) {
-  return Bytes.fromHex(value.substring(2))
-}
+import BN from 'bn.js'
 
 describe('rlpEncode', () => {
-  for (const testCase of testCases) {
-    it(testCase.name, () => {
-      const result = rlpEncode(testCase.decoded)
-      expect(result).to.deep.equal(testCase.encoded)
-    })
-  }
+  describe('ethers', () => {
+    for (const testCase of testCasesEthers) {
+      it(testCase.name, () => {
+        const result = rlpEncode(fromEthers(testCase.decoded))
+        expect(result).to.deep.equal(Bytes.fromHex(testCase.encoded))
+      })
+    }
+  })
+
+  describe('eth valid', () => {
+    for (const [name, testCase] of Object.entries(testCasesEthValid)) {
+      it(name, () => {
+        const result = rlpEncode(fromEth(testCase.in))
+        expect(result).to.deep.equal(Bytes.fromHex(testCase.out))
+      })
+    }
+  })
 })
 
 describe('rlpDecode', () => {
-  for (const testCase of testCases) {
-    it(testCase.name, () => {
-      const result = rlpDecode(testCase.encoded)
-      expect(result).to.deep.equal(testCase.decoded)
-    })
-  }
+  describe('ethers', () => {
+    for (const testCase of testCasesEthers) {
+      it(testCase.name, () => {
+        const result = rlpDecode(Bytes.fromHex(testCase.encoded))
+        expect(result).to.deep.equal(fromEthers(testCase.decoded))
+      })
+    }
+  })
+
+  describe('eth valid', () => {
+    for (const [name, testCase] of Object.entries(testCasesEthValid)) {
+      it(name, () => {
+        const result = rlpDecode(Bytes.fromHex(testCase.out))
+        expect(result).to.deep.equal(fromEth(testCase.in))
+      })
+    }
+  })
+
+  describe('eth invalid', () => {
+    for (const [name, testCase] of Object.entries(testCasesEthInvalid)) {
+      it(name, () => {
+        expect(() => rlpDecode(Bytes.fromHex(testCase.out))).to.throw()
+      })
+    }
+  })
 })
+
+type EthInput = string | number | EthInput[]
+function fromEth(input: EthInput): RlpSerializable {
+  if (typeof input === 'number') {
+    return Bytes.fromNumber(input)
+  } else if (typeof input === 'string') {
+    if (input.startsWith('#')) {
+      const hex = new BN(input.substring(1), 10).toString(16)
+      return Bytes.fromHex(hex)
+    } else {
+      return Bytes.fromByteArray(input.split('').map((x) => x.charCodeAt(0)))
+    }
+  } else {
+    return input.map(fromEth)
+  }
+}
+
+type EthersInput = string | EthersInput[]
+function fromEthers(value: EthersInput): RlpSerializable {
+  if (Array.isArray(value)) {
+    return value.map(fromEthers)
+  } else {
+    return Bytes.fromHex(value)
+  }
+}
